39947552d7879cfa67bd173ee958b7d158096f11
Salam Mokaid <salam.mokaid@gmail.com>
2024-06-06 10:20:06 +0200

    changes

--- app/static/app/js/education.js
+++ app/static/app/js/education.js
@@ -1,4 +1,3 @@
-import { Dropdown } from 'flowbite';
 const { Dropdown } = require("flowbite");
 
 // jQuery ready function to ensure the DOM is fully loaded
@@ -35,10 +34,9 @@ $(document).ready(function() {
 
         // Send AJAX request
         $.ajax({
-            url: '/add-education/',
+            url: '{% url "add_education" %}',
             type: 'POST',
             data: formData,
-            contentType: false,
             processData: false,
            
             success: function(data) {
@@ -54,14 +52,14 @@ $(document).ready(function() {
                             
                             <!--  Add edit and delete button -->
                                 <div class="flex justify-end px-4 pt-4">
-                                <button id="dropdownButton-${data.data.id}" data-dropdown-toggle="dropdown-${data.data.id}" class="inline-flex items-center p-2 text-sm font-medium text-center text-gray-900 bg-white rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none dark:text-white focus:ring-gray-50 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-600" type="button"> 
+                                <button id="dropdownButton-" data-dropdown-toggle="dropdown" class="inline-flex items-center p-2 text-sm font-medium text-center text-gray-900 bg-white rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none dark:text-white focus:ring-gray-50 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-600" type="button"> 
                                 <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 3">
                                   <path d="M2 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm6.041 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM14 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Z"/>
                                 </svg>
                               </button>
                               
                               <!-- Dropdown menu -->
-                              <div id="dropdown-${data.data.id}" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
+                              <div id="dropdown" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
                                   <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownMenuIconHorizontalButton">
                                     <li>
                                       <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
