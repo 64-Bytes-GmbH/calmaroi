7ba213ae614114c0d3ac26b0c0ede6f62a0aa29b
Salam Mokaid <salam.mokaid@gmail.com>
2024-06-18 12:14:21 +0200

    added template pages and navigation inside an application

--- app/dashboard_urls.py
+++ app/dashboard_urls.py
@@ -4,18 +4,65 @@ from django.urls import path
 from app import dashboard_views as views
 
 urlpatterns = [
-    path("", views.dashboard, name="dashboard"),
-    path("Bewerbungen/", views.all_applications, name="all_applications"),
+    ######### general dashboard #########
     path(
-        "Bewerbung/<str:user_id>/User-Profile/",
-        views.application_overview,
-        name="application_overview",
+        "", 
+        views.dashboard,
+        name="dashboard"
     ),
-    path("Bewerbung/<str:user_id>Zeitplan/", views.career_review, name="career_review"),
     path(
-        "Bewerbung/<str:user_id>/Berufliche-Vorerfahrungen",
-        views.personal_review,
-        name="personal_review",
+        "Bewerbungen/",
+        views.all_applications,
+        name="all_applications"
     ),
-    path("Bewerbung/<str:user_id>/Persönlichkeit", views.timeplan, name="timeplan"),
+
+    ######### single application #########
+    # general
+    path(
+        "Bewerbung/<int:user_id>/Unterlagen/",
+        views.application_documents,
+        name="application_documents",
+    ),
+    path(
+        "Bewerbung/<int:user_id>/Zeitplan/", 
+        views.application_timeline,
+        name="application_timeline"
+    ),
+    path(
+        "Bewerbung/<int:user_id>/Wohnraum/", 
+        views.application_living_space,
+        name="application_living_space"
+    ),
+    path(
+        "Bewerbung/<int:user_id>/Betreuung/", 
+        views.application_assisstance,
+        name="application_assisstance"
+    ),
+    # evaluation
+    path(
+        "Bewerbung/<int:user_id>/Berufliche-Vorerfahrungen",
+        views.application_career_evaluation,
+        name="application_career_evaluation",
+    ),
+    path(
+        "Bewerbung/<int:user_id>/Persönlichkeit/",
+        views.application_personality_evaluation,
+        name="application_personality_evaluation",
+    ),
+    # more
+    path(
+        "Bewerbung/<int:user_id>/Ansprechpartner/", 
+        views.application_contact_person,
+        name="application_contact_person"
+    ),
+    path(
+        "Bewerbung/<int:user_id>/Interne-Kommunikation/", 
+        views.application_internal_communication,
+        name="application_internal_communication"
+    ),
+    path(
+        "Bewerbung/<int:user_id>/Notizen/", 
+        views.application_notes,
+        name="application_notes"
+    ),
 ]
--- app/dashboard_views.py
+++ app/dashboard_views.py
@@ -2,56 +2,145 @@
 
 from django.shortcuts import render
 
+
+############### general dashboard ###############
 def dashboard(request):
-    """ Startseite """
+    """Startseite"""
 
     context = {}
 
-    response = render(request, 'dashboard/dashboard.html', context)
+    response = render(request, "dashboard/dashboard.html", context)
 
     return response
+
 
 def all_applications(request):
-    """ Alle Anträge """
+    """Alle Anträge"""
 
     context = {}
 
-    response = render(request, 'dashboard/all_applications.html', context)
+    response = render(request, "dashboard/all_applications.html", context)
 
     return response
 
-def application_overview(request):
-    """ Antragsübersicht """
+
+############### single application ###############
+
+
+### general ###
+def application_documents(request, user_id):
+    """Übersicht der Dokumente"""
 
     context = {}
 
-    response = render(request, 'dashboard/application_overview.html', context)
+    context["user_id"] = user_id
+
+    response = render(request, "dashboard/application/general/documents.html", context)
 
     return response
 
-def career_review(request):
-    """ Bewerbung prüfen """
+
+def application_timeline(request, user_id):
+    """Zeitplan"""
 
     context = {}
 
-    response = render(request, 'dashboard/career_review.html', context)
+    context["user_id"] = user_id
+
+    response = render(request, "dashboard/application/general/timeline.html", context)
 
     return response
 
-def personal_review(request):
-    """ Persönliche Daten prüfen """
+
+def application_living_space(request, user_id):
+    """Wohnraum"""
 
     context = {}
 
-    response = render(request, 'dashboard/personal_review.html', context)
+    context["user_id"] = user_id
+
+    response = render(
+        request, "dashboard/application/general/living_space.html", context
+    )
 
     return response
 
-def timeplan(request):
-    """ Zeitplan """
+
+def application_assisstance(request, user_id):
+    """Betreuung"""
 
     context = {}
 
-    response = render(request, 'dashboard/timeplan.html', context)
+    context["user_id"] = user_id
+
+    response = render(
+        request, "dashboard/application/general/assisstance.html", context
+    )
+
+    return response
+
+
+### evaluation ###
+def application_career_evaluation(request, user_id):
+    """Berufliche Vorerfahrungen bewerten"""
+
+    context = {}
+
+    context["user_id"] = user_id
+
+    response = render(
+        request, "dashboard/application/evaluation/career_evaluation.html", context
+    )
+
+    return response
+
+
+def application_personality_evaluation(request, user_id):
+    """Persönlichkeit bewerten"""
+
+    context = {}
+
+    context["user_id"] = user_id
+
+    response = render(
+        request, "dashboard/application/evaluation/personality_evaluation.html", context
+    )
+
+    return response
+
+
+### more ###
+def application_contact_person(request, user_id):
+    """Ansprechpartner"""
+
+    context = {}
+
+    context["user_id"] = user_id
+
+    response = render(request, "dashboard/application/more/contact_person.html", context)
+
+    return response
+
+
+def application_internal_communication(request, user_id):
+    """Interne Kommunikation"""
+
+    context = {}
+
+    context["user_id"] = user_id
+
+    response = render(request, "dashboard/application/more/internal_communication.html", context)
+
+    return response
+
+
+def application_notes(request, user_id):
+    """Notizen"""
+
+    context = {}
+
+    context["user_id"] = user_id
+
+    response = render(request, "dashboard/application/more/notes.html", context)
 
     return response
