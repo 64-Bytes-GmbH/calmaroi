6f6958b2f03d79993b68e11e61021234fb4967de
Salam Mokaid <salam.mokaid@gmail.com>
2024-06-20 12:32:39 +0200

    matched urls

--- app/dashboard_urls.py
+++ app/dashboard_urls.py
@@ -19,49 +19,49 @@ urlpatterns = [
     ######### single application #########
     # general
     path(
-        "Bewerbung/<int:user_id>/Unterlagen/",
+        "Bewerbungen/<int:user_id>/Unterlagen/",
         views.application_documents,
         name="application_documents",
     ),
     path(
-        "Bewerbung/<int:user_id>/Zeitplan/", 
+        "Bewerbungen/<int:user_id>/Zeitplan/", 
         views.application_timeline,
         name="application_timeline"
     ),
     path(
-        "Bewerbung/<int:user_id>/Wohnraum/", 
+        "Bewerbungen/<int:user_id>/Wohnraum/", 
         views.application_living_space,
         name="application_living_space"
     ),
     path(
-        "Bewerbung/<int:user_id>/Betreuung/", 
+        "Bewerbungen/<int:user_id>/Betreuung/", 
         views.application_assisstance,
         name="application_assisstance"
     ),
     # evaluation
     path(
-        "Bewerbung/<int:user_id>/Berufliche-Vorerfahrungen",
+        "Bewerbungen/<int:user_id>/Berufliche-Vorerfahrungen",
         views.application_career_evaluation,
         name="application_career_evaluation",
     ),
     path(
-        "Bewerbung/<int:user_id>/Persönlichkeit/",
+        "Bewerbungen/<int:user_id>/Persönlichkeit/",
         views.application_personality_evaluation,
         name="application_personality_evaluation",
     ),
     # more
     path(
-        "Bewerbung/<int:user_id>/Ansprechpartner/", 
+        "Bewerbungen/<int:user_id>/Ansprechpartner/", 
         views.application_contact_person,
         name="application_contact_person"
     ),
     path(
-        "Bewerbung/<int:user_id>/Interne-Kommunikation/", 
+        "Bewerbungen/<int:user_id>/Interne-Kommunikation/", 
         views.application_internal_communication,
         name="application_internal_communication"
     ),
     path(
-        "Bewerbung/<int:user_id>/Notizen/", 
+        "Bewerbungen/<int:user_id>/Notizen/", 
         views.application_notes,
         name="application_notes"
     ),
